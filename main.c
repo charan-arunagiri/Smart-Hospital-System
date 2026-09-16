a40bc12e0effcc41ef0c6f8f35daa9fe09631415
Arunagiri Charan <as20250385@sci.sjp.ac.lk>
2026-09-16 16:04:14 +0530

    Menu

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;
+    do {
+        printf("\n--------------- SMART HOSPITAL SYSTEM --------------\n");
+        printf("   1. Register New Patient\n");
+        printf("   2. Display Bed Occupancy\n");
+        printf("   3. Display Patients by Priority (Triage)\n");
+        printf("   4. Generate Summary Report\n");
+        printf("   5. Save & Exit\n");
+        printf("------------------------------------------------------\n");
+        printf("  Enter your choice: ");
+        printf("\n------------------------------------------------------\n");
+        scanf("%d", &choice);
+    }
+    while (choice != 5);
+    return 0;
+}
