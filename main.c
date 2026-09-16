34b8b740800f03facf7702d629e6ab09b3be4bfd
Arunagiri Charan <as20250385@sci.sjp.ac.lk>
2026-09-17 00:12:11 +0530

    Update Menu

--- main.c
+++ main.c
@@ -16,6 +16,30 @@ int main()
         printf("\n------------------------------------------------------\n");
         scanf("%d", &choice);
     }
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input. Exiting...\n");
+            break;
+        }
+
+        switch (choice) {
+            case 1:
+                registerPatient();
+                break;
+            case 2:
+                printf("Display bed occupancy pending implementation.\n");
+                break;
+            case 3:
+                printf("Triage display pending implementation.\n");
+                break;
+            case 4:
+                printf("Summary report pending implementation.\n");
+                break;
+            case 5:
+                printf("Saving state and exiting system...\n");
+                break;
+            default:
+                printf("Invalid choice, please try again.\n");
+        }
     while (choice != 5);
     return 0;
 }
